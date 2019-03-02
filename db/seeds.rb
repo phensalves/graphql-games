a010c06556611e77ebb9fe804e01725a7ac24c6b
Pedro Alves <pedro_alves@me.com>
2019-03-02 18:30:21 -0300

    populating db through seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# personagens
+mario = Character.create(name: "Mario")
+peach = Character.create(name: "Peach")
+dk = Character.create(name: "Donkey Kong")
+zelda = Character.create(name: "Zelda")
+ 
+# games
+mario_world = Game.create(name: "Super Mario World", description: "SNES game", launch_year: "1990", characters: [mario, peach])
+mario_kart = Game.create(name: "Mario Kart 8", description: "Wii U Game", launch_year: "2014", characters: [mario, peach, dk])
+smash_bros = Game.create(name: "Super Smash Bros Wii U", description: "Wii U Game", launch_year: "2014", characters: [mario, peach, dk, zelda])
+# adicionando games aos personagens
+mario.games << [mario_world, mario_kart, smash_bros]
+mario.save
+ 
+peach.games << [mario_world, mario_kart, smash_bros]
+peach.save
+ 
+zelda.games << [smash_bros]
+zelda.save
+ 
+dk.games << [mario_kart, smash_bros]
+dk.save
