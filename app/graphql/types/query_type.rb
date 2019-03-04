c59c1e1fd15470d4ec47474356cbecd8d65c477a
Pedro Alves <pedro_alves@me.com>
2019-03-04 18:39:35 -0300

    WIP (building query type to enable query, but need fix)

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,11 +1,9 @@
-module Types
-  class QueryType < Types::BaseObject
-    field :game do
-      type Types::GameType
-      argument :id, !types.Int
-      resolve -> (root, args, ctx) {
-        Game.find(args[:id])
-      }
-    end
+Types::QueryType = GraphQL::ObjectType.define do
+  field :game do
+    type Types::GameType
+    argument :id, !types.Int
+    resolve -> (root, args, ctx) {
+      Game.find(args[:id])
+    }
   end
 end
